b534e11aa6fe7960c460b891c5345472b508380b
SoliiMbu <70231735+2shoobie@users.noreply.github.com>
2025-01-17 22:48:14 -0500

    Add files via upload

---
+++ StudentExam(SQL1).sql
@@ -0,0 +1,9 @@
+SELECT gender, math_score, reading_score, writing_score, parental_level_of_education, test_preparation_course
+FROM StudentExam
+
+ORDER BY reading_score DESC
+
+
+-- SELECT gender, math_score, reading_score, writing_score FROM StudentExam ORDER BY reading_score ASC
+-- SELECT gender, math_score, reading_score, writing_score FROM StudentExam ORDER BY math_score ASC
+-- SELECT gender, math_score, reading_score, writing_score FROM StudentExam ORDER BY writing_score ASC
---
+++ StudentExamAverage.sql
@@ -0,0 +1,6 @@
+SELECT AVG(writing_score)
+FROM StudentExam
+
+-- SELECT AVG(math_score) FROM StudentExam 
+
+-- SELECT AVG(reading_score) FROM StudentExam
